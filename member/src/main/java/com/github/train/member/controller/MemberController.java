5da130719cf07a6fe90efd5acd98ecfb9360c8c6
zy_sun <08183039@cumt.edu.cn>
2024-01-18 17:17:37 +0800

    feat: ✨ 添加一异常类，工具类，会员登录，发送验证码等功能

---
+++ member/src/main/java/com/github/train/member/controller/MemberController.java
@@ -0,0 +1,48 @@
+package com.github.train.member.controller;
+
+
+import com.github.train.common.resp.CommonResp;
+import com.github.train.member.req.MemberLoginReq;
+import com.github.train.member.req.MemberRegisterReq;
+import com.github.train.member.req.MemberSendCodeReq;
+import com.github.train.member.resp.MemberLoginResp;
+import com.github.train.member.service.MemberService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+
+
+    @Resource
+    private MemberService memberService;
+
+    @GetMapping("/count")
+    public CommonResp<Integer> count() {
+        int count = (int) memberService.count();
+        CommonResp<Integer> commonResp = new CommonResp<>();
+        commonResp.setContent(count);
+        return commonResp;
+    }
+
+    @PostMapping("/register")
+    public CommonResp<Long> register(@Valid MemberRegisterReq req) {
+        long register = memberService.register(req);
+        return new CommonResp<>(register);
+    }
+
+    // @PostMapping("/send-code")
+    // public CommonResp<Long> sendCode(@Valid @RequestBody MemberSendCodeReq req) {
+    //     memberService.sendCode(req);
+    //     return new CommonResp<>();
+    // }
+
+    // @PostMapping("/login")
+    // public CommonResp<MemberLoginResp> login(@Valid @RequestBody MemberLoginReq req) {
+    //     MemberLoginResp resp = memberService.login(req);
+    //     return new CommonResp<>(resp);
+    // }
+}
