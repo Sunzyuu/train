25df5cbfe3046cc720e03b94cf8dfa41db300221
zy_sun <08183039@cumt.edu.cn>
2024-01-18 13:37:50 +0800

    feat: ✨ 添加公共模块，修改pom依赖

--- member/src/main/java/com/github/train/member/MemberApplication.java
+++ member/src/main/java/com/github/train/member/MemberApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+
 @SpringBootApplication
+@ComponentScan("com.github")
 public class MemberApplication {
 
     public static void main(String[] args) {
