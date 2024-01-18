8f186d824256223e9d8bbdbbcd77c28268d63366
zy_sun <08183039@cumt.edu.cn>
2024-01-18 15:07:36 +0800

    feat: ✨ 集成mybatis代码生成器

--- member/src/main/java/com/github/train/member/MemberApplication.java
+++ member/src/main/java/com/github/train/member/MemberApplication.java
@@ -1,5 +1,6 @@
 package com.github.train.member;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.github")
+@MapperScan("com.github.train.*.mapper")
 public class MemberApplication {
 
     public static void main(String[] args) {
